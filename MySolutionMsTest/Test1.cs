38bbad68882d992e4ade82dc9e6602ee90788603
zniska <georg.staravoitau@gmail.com>
2026-09-21 02:52:19 +0300

    Add MySolutions

---
+++ MySolutionMsTest/Test1.cs
@@ -0,0 +1,43 @@
+﻿namespace MySolutionMsTest;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestInitialize]
+    public void Initialize()
+    {
+        Console.WriteLine("TestInitialize");
+    }
+
+    [ClassInitialize]
+    public static void ClassInitialize(TestContext testContext)
+    {
+        Console.WriteLine("ClassInitialize");
+    }
+    
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Console.WriteLine("Test");
+        Assert.AreEqual("test", "netest", "Test was failed");
+
+    }
+
+    [TestMethod]
+    public void TestMethod2()
+    {
+        Console.WriteLine("Test");
+    }
+    
+    [TestCleanup]
+    public void Cleanup()
+    {
+        Console.WriteLine("TestCleanup");
+    }
+
+    [ClassCleanup]
+    public static void ClassCleanup()
+    {
+        Console.WriteLine("ClassCleanup");
+    }
+}
---
+++ MySolutionsXUnit/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace MySolutionsXUnit;
+
+public class UnitTest1 
+{
+    public UnitTest1()
+    {
+        Console.WriteLine("UnitTest1");
+    }
+    
+    [Fact]
+    public void Test1()
+    {
+        Console.WriteLine("Test1");
+    }
+    
+    public void Dispose()
+    {
+        Console.WriteLine("Dispose");
+    }
+}
